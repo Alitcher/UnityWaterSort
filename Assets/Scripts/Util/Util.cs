711eab99a4c98087a6e4f6f020b55c5e39b824d2
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2023-11-11 22:51:29 +0200

    shuffle logic

---
+++ Assets/Scripts/Util/Util.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+public class Util 
+{
+    private static Random rng = new Random();
+    public static void Shuffle<T>(IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
