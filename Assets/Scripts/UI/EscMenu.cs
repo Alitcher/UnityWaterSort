6dd5bf1d819233a1cad3d4a0667f7585081de15d
mihkelroomet <mihkelroomet@gmail.com>
2023-11-12 07:24:47 +0200

    Added restart button to in-game popup menu

--- Assets/Scripts/UI/EscMenu.cs
+++ Assets/Scripts/UI/EscMenu.cs
@@ -5,13 +5,20 @@ using UnityEngine.SceneManagement;
 public class EscMenu : MonoBehaviour
 {
     // Buttons
+    [SerializeField] private Button RestartButton;
     [SerializeField] private Button MenuButton;
     [SerializeField] private Button QuitButton;
 
     private void Awake()
     {
+        RestartButton.onClick.AddListener(() => RestartGame());
         MenuButton.onClick.AddListener(() => BackToMenu());
         QuitButton.onClick.AddListener(() => QuitGame());
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
     }
 
     public void BackToMenu()
